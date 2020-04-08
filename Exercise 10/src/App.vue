5689d604fe1c52c28e76050b1cc4040876e40394
leszekjanczewski <leszek.janczewski@gmail.com>
2020-04-08 18:42:24 +0200

    exercise 10 - finished

---
+++ Exercise 10/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 v-myOn:click="allertMe">Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            allertMe() {
+                alert('No CLICK!')
+            }
+        },
+        directives: {
+            'myOn': {
+                bind(el, binding, vnode) {
+                    let type = binding.arg;
+                    let myFunction = binding.value;
+                    el.addEventListener(type, myFunction);
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
